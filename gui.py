f1364a053855f971b18631d78361295eb8bb743a
Nauf <130215373+naufudge@users.noreply.github.com>
2023-11-23 13:56:35 +0500

    added calendar instead of entering file name

--- gui.py
+++ gui.py
@@ -2,38 +2,125 @@ import httpx, threading, asyncio
 from doc import doc
 import tkinter as tk
 from tkinter import ttk
+from tkcalendar import *
+from hijridate import Gregorian
+from datetime import datetime
 from async_scrapping import NewsScrapping
+
+weekday_to_dhivehi = {
+    0: "ހޯމަ",
+    1: "އަންގާރަ",
+    2: "ބުދަ",
+    3: "ބުރާސްފަތި",
+    4: "ހުކުރު",
+    5: "ހޮނިހިރު",
+    6: "އާދިއްތަ"
+}
+
+month_num = {
+    'jan' : 1,
+    'feb' : 2,
+    'mar' : 3,
+    'apr' : 4,
+    'may' : 5,
+    'jun' : 6,
+    'jul' : 7,
+    'aug' : 8,
+    'sep' : 9,
+    'oct' : 10,
+    'nov' : 11,
+    'dec' : 12,
+}
+
+short_to_long_month = {
+    'jan' : 'january',
+    'feb' : 'february',
+    'mar' : 'march',
+    'apr' : 'april',
+    'may' : 'may',
+    'jun' : 'june',
+    'jul' : 'july',
+    'aug' : 'august',
+    'sep' : 'september',
+    'oct' : 'october',
+    'nov' : 'november',
+    'dec' : 'december',
+}
+
+hijri_num_to_month = {
+    1: "މުޙައްރަމް",
+    2: "ޞަފަރު",
+    3: "ރަބީޢުލްއައްވަލް",
+    4: "ރަބީޢުލްއާޚިރު",
+    5: "ޖުމާދުލްއޫލާ",
+    6: "ޖުމާދަލްއާޚިރާ",
+    7: "ރަޖަބު",
+    8: "ޝަޢުބާން",
+    9: "ރަމަޟާން",
+    10: "ޝައްވާލް",
+    11: "ޛުލްޤައިދާ",
+    12: "ޛުލްޙިއްޖާ",
+}
+
+month_num_in_dhivehi = {
+    'ޖަނަވަރީ': 1,
+    'ފެބުރުވަރީ' : 2,
+    'މާރިޗު' : 3,
+    'އޭޕްރިލް' : 4,
+    'މޭ' : 5,
+    'ޖޫން' : 6,
+    'ޖުލައި' : 7,
+    'އޮގަސްޓް' : 8,
+    'ސެޕްޓެންބަރު': 9,
+    'އޮކްޓޫބަރު' : 10,
+    'ނޮވެންބަރު': 11,
+    'ޑިސެންބަރު' : 12
+}
+
 
 class Scrapper:
     def __init__(self, async_loop):
         self.root = tk.Tk()
         self.async_loop = async_loop
 
-        self.root.title("EK Scrapper v1.2")
+        self.root.title("EK Scrapper")
         try:
             self.root.iconbitmap('icon.ico')
         except Exception as e:
             print(e)
 
         self.label = tk.Label(self.root, text="އެކިއެކި ކަންކަން ހިނގާ ގޮތް ނެގުމަށްޓަކައި", font=('Faruma', 20))
-        self.label.pack(padx=20, pady=20)
+        self.label.grid(row=0, column=0, columnspan=2)
 
-        self.textbox = tk.Text(self.root, height=3, font=('Faruma', 14), borderwidth=2)
-        self.textbox.pack(padx=20, pady=10)
+        self.textbox = tk.Text(self.root, height=3, font=('Faruma', 13), borderwidth=2)
+        self.textbox.grid(row=2, column=0, padx=20, pady=5, columnspan=2)
 
-        self.entry_label = tk.Label(self.root, text="ތިރީގައި ފައިލް ނަން ލިޔުއްވާ", font=('Faruma', 15)).pack()
+        self.date = tk.Label(self.root, text="ތާރީޚު ނަންގަވާ", font=('Faruma', 14))
+        self.date.grid(row=3, column=0, pady=5)
 
-        self.file_name = tk.StringVar
-        self.entry = tk.Entry(self.root, width=35, borderwidth=2, font=('Faruma', 13), textvariable=self.file_name)
-        self.entry.pack(padx=20, pady=10)
+        self.calendar = Calendar(self.root,
+                                 selectmode="day",
+                                 firstweekday="sunday",
+                                 weekenddays=[6,7],
+                                 font=('Faruma', 10),
+                                 showweeknumbers=False,
+                                 date_pattern='dd/MM/yyyy'
+                                )
+        self.calendar.grid(row=4, column=0, pady=10, padx=20)
+
+        # self.entry_label = tk.Label(self.root, text="ތިރީގައި ފައިލް ނަން ލިޔުއްވާ", font=('Faruma', 15))
+        # self.entry_label.grid()
+        # self.entry = tk.Entry(self.root, width=35, borderwidth=2, font=('Faruma', 14), textvariable=self.file_name)
+        # self.entry.pack(padx=20, pady=10)
+
+        self.updates = tk.Label(self.root, text="އައުޓްޕުޓް", font=('Faruma', 14))
+        self.updates.grid(row=3, column=1)
+
+        self.progress = tk.Text(self.root, height=10, width=70, state='disabled', font=('Calibri', 12))
+        self.progress.grid(row=4, column=1, padx=20)
 
         self.button1 = ttk.Button(self.root, width=15, text="\n!ނަގާ ޚަބަރު\n", command= lambda: self.do_tasks())
-        self.button1.pack(padx=10, pady=10)
-
-        self.updates = tk.Label(self.root, text="Progress Updates:", font=('Faruma', 15))
-        self.updates.pack()
-        self.progress = tk.Text(self.root, height=10, width=70, state='disabled', font=('Calibri', 12))
-        self.progress.pack(pady=10)
+        self.button1.grid(row=5, column=1, pady=10)
 
         self.root.mainloop()
 
@@ -42,7 +129,35 @@ class Scrapper:
 
     def do_tasks(self):
         """ Button-Event-Handler starting the asyncio part. """
+
+        self.file_name = self.calendar.get_date()
         threading.Thread(target=self._asyncio_thread).start()
+
+    def get_file_name(self, date: list):
+        """Returns the filename based on the date [dd,MM,yyyy] passed in."""
+        months = {v: k for k, v in month_num.items()}
+        day = int(date[0])
+        if day < 10:
+            day = f"0{date[0]}"
+        month = short_to_long_month[months[int(date[1])]].capitalize()
+
+        return f"{day} {month} {date[2]}"
+
+    def get_greg_date(self, date: list):
+        """Returns Gregorian Date [dd, MM, yyy] converted to Dhivehi as a string"""
+        date = [int(num) for num in date]
+        months = {v: k for k,v in month_num_in_dhivehi.items()}
+        Date = datetime(date[2], date[1], date[0]).weekday()
+        return f"{date[0]} {months[int(date[1])]} {date[2]} ({weekday_to_dhivehi[Date]})"
+
+    def get_hijri_date(self, date: list):
+        """Returns Gregorian Date [dd, MM, yyy] converted into Hijri Date in Dhivehi as a string"""
+        date = [int(num) for num in date]
+        converted_date = str(Gregorian(date[2], date[1], date[0]).to_hijri())
+        converted_date_list = converted_date.split('-')
+        converted_date_list[1] = hijri_num_to_month[int(converted_date_list[1])]
+        
+        return ' '.join(reversed(converted_date_list))
 
     # Progress updater below
     def printer(self, msg):
@@ -57,14 +172,20 @@ class Scrapper:
         urls.append(inputt)
 
         new_urls = [url.strip() for sublist in urls for url in sublist]
-        file_name = self.entry.get()
+        date = self.calendar.get_date().split('/')
+        file_name = self.get_file_name(date)
+
+        hijri_date = self.get_hijri_date(date)
+        dhivehi_date = self.get_greg_date(date)
+
         if not new_urls[0]:
             self.printer("No links. Paste the link you would like to take.")
             return
         if not file_name:
-            self.printer("Enter a file name.")
+            self.printer("Please choose a date.")
             return
-        timeout = httpx.Timeout(5, read=None)
+
+        timeout = httpx.Timeout(5, connect=None, read=None)
         async with httpx.AsyncClient(http2=True, timeout=timeout) as client:
             ns = NewsScrapping(client)
 
@@ -80,6 +201,7 @@ class Scrapper:
 
                 elif (url.find("avas.mv") > 0):
                     tasks.append(asyncio.create_task(ns.Avas(url)))
+
                 else:
                     self.printer(f"'{url}' isn't a supported link. Please enter a suitable link.")
                     continue
@@ -89,7 +211,9 @@ class Scrapper:
             #BELOW HERE IS INPUTING THE DATA TO DOCX FILE
             for result in results:
                 update_msg = doc(
-                    filename = file_name, 
+                    filename = file_name,
+                    hijri_date=hijri_date,
+                    dhivehi_date=dhivehi_date,
                     url = result['url'], 
                     author = result['author'], 
                     title = result['title'], 
